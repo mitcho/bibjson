13add18b4600d40b8435dae31369b5e9f66c6b46
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2014-12-21 21:34:18 -0500

    fix some other latex chars; process authors; include a few other fields

--- bibjson.js
+++ bibjson.js
@@ -13,29 +13,63 @@ paperarchive = paperarchive.replace(/@comment{BibDesk Static Groups{(.|\n)*}}/m,
 
 var bib = parser.toJSON(paperarchive);
 
+var authorUrls = {
+	'van Urk, Coppe': 'http://web.mit.edu/cvanurk/www/',
+	'Levin, Theodore': 'https://sites.google.com/site/tfranklevin/',
+	'Kotek, Hadas': 'http://hkotek.com'
+};
+
 function cleanup(text) {
 	// todo: it would be great if this could be automated somehow:
  	text = text.replace('{\\v \\i}','ǐ'); // for bǐ
  	text = text.replace('{\\u \\a}','ă');
  	text = text.replace('{\\c \\s}','ş');
+
+ 	text = text.replace('\\&','&amp;');
  	
 	text = text.replace('\\VAN{Urk}', 'van Urk'); // for Coppe
-	text = text.replace(/{(\w)}/g,'$1');
+	text = text.replace(/{(\w+)}/g,'$1');
 	text = text.replace(/{\\em ([^}]*)}/g,'<em>$1</em>');
+	
 	return text;
+}
+
+function parseAuthors(text) {
+	var rawAuthors = text.split(' and ');
+	var authors = [];
+	for ( i in rawAuthors ) {
+		var author = rawAuthors[i];
+		var authorParts = author.split(', ');
+		var item = {
+			'name': author,
+			'lastName': authorParts[0],
+			'displayName': authorParts[1] + ' ' + authorParts[0]
+		};
+		if ( author in authorUrls ) {
+			item.url = authorUrls[author];
+		}
+		authors.push(item);
+	}
+	return authors;
 }
 
 function convertItem(item) {
 	var newItem = {
 		citationKey: item.citationKey.toLowerCase(),
 		entryType: item.entryType.toLowerCase(),
-		keywords: []
+		keywords: [],
+		authors: []
 	};
-	var keys = ['AUTHOR','EDITOR','TITLE','URL','NOTE','YEAR'];
+	var keys = ['AUTHOR', 'EDITOR', 'TITLE', 'URL', 'NOTE', 'YEAR', 'ABSTRACT', 'BOOKTITLE', 'JOURNAL', 'PAGES', 'SCHOOL'];
 	for (i in keys) {
 		if (keys[i] in item.entryTags)
-			newItem[keys[i].toLowerCase()] = cleanup(item.entryTags[keys[i]]);
+			newItem[keys[i].toLowerCase()] = cleanup(item.entryTags[keys[i]], keys[i]);
 	}
+	
+	if ('AUTHOR' in item.entryTags) {
+		newItem['authors'] = parseAuthors(cleanup(item.entryTags['AUTHOR']));
+	}
+	
 	if ('KEYWORDS' in item.entryTags)
 		newItem.keywords = item.entryTags.KEYWORDS.split(/, */);
 	return newItem;
